8405c745a2228670f195b16a40e47ccd2c0e9c83
Alghareb1997 <164238122+Alghareb1997@users.noreply.github.com>
2024-10-05 00:14:36 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Execp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int num1 = 10;
+                int num2 = 0;
+                int num3 = num1 / num2; ;
+            }
+            catch
+            {
+                Console.WriteLine("lagic error");
+            }
+            finally
+            {
+                Console.WriteLine("execption handling");
+            }
+            //another shape for it .....
+            try
+            {
+                int num1 = 10;
+                int num2 = 0;
+                int num3 = num1 / num2; ;
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine("lagic error");
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("lagic error");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("hello from execption");
+            }
+            finally
+            {
+                Console.WriteLine("execption handling");
+            }
+
+
+
+
+
+        }
+    }
+}
